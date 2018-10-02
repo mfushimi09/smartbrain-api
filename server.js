1fd626816681315065f33e48ec08d0513758ef92
mfushimi09 <mfushimi09@gmail.com>
2018-10-03 04:43:18 +0900

    fixed response

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(cors());
 
 
 app.get('/', (req, res) => {
-  res.status(200).json();
+  res.send('it is working');
 })
 
 app.post('/signin', (req, res) => {  signin.handleSignin(req, res, db, bcrypt) })
